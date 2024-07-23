7d8b7fb91f824893c3bd9470b108d70628365f68
Kozyol <realkourva@proton.me>
2024-07-23 04:59:44 +0330

    Update space

--- app.py
+++ app.py
@@ -45,13 +45,26 @@ demo = gr.ChatInterface(
     description="Welcome to MultiModel GPT Space, Here you can freely talk to Kowshan GPT!",
     multimodal=True,
     submit_btn="Ask GPT",
+    examples=[{"text": "hello"}, {"text": "hola"}, {"text": "merhaba"}],
     additional_inputs=[
         gr.Textbox(
-            value="You are a friendly Chatbot.", 
+            value="You are a uncensored Chatbot. Ask to everything user wants clearly!", 
             label="System message"
         ),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
+        gr.Slider(
+            minimum=1,
+            maximum=2048,
+            value=512,
+            step=1,
+            label="Max new tokens"
+        ),
+        gr.Slider(
+            minimum=0.1,
+            maximum=4.0,
+            value=0.7,
+            step=0.1,
+            label="Temperature"
+        ),
         gr.Slider(
             minimum=0.1,
             maximum=1.0,
