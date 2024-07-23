208727f02be9deaf128c570bde9d851aaf0019dd
Kozyol <realkourva@proton.me>
2024-07-23 04:38:09 +0330

    Update space

--- app.py
+++ app.py
@@ -1,41 +1,38 @@
+# Imports
 import gradio as gr
 from huggingface_hub import InferenceClient
 
-"""
-For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
-"""
+# Client
 client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 
-
-def respond(
-    message,
-    history: list[tuple[str, str]],
-    system_message,
-    max_tokens,
-    temperature,
-    top_p,
-):
+# Chat GPT generator
+def respond(message,
+            history: list[tuple[str, str]],
+            system_message,
+            max_tokens,
+            temperature,
+            top_p):
+    # System message
     messages = [{"role": "system", "content": system_message}]
 
+    # History
     for val in history:
         if val[0]:
             messages.append({"role": "user", "content": val[0]})
         if val[1]:
             messages.append({"role": "assistant", "content": val[1]})
 
+    # User prompt
     messages.append({"role": "user", "content": message})
 
+    # GPT response
     response = ""
-
-    for message in client.chat_completion(
-        messages,
-        max_tokens=max_tokens,
-        stream=True,
-        temperature=temperature,
-        top_p=top_p,
-    ):
+    for message in client.chat_completion(messages,
+                                          max_tokens=max_tokens,
+                                          stream=True,
+                                          temperature=temperature,
+                                          top_p=top_p):
         token = message.choices[0].delta.content
-
         response += token
         yield response
 
@@ -43,9 +40,16 @@ def respond(
 For information on how to customize the ChatInterface, peruse the gradio docs: https://www.gradio.app/docs/chatinterface
 """
 demo = gr.ChatInterface(
-    respond,
+    fn=respond,
+    title="Kowshan GPT",
+    description="Welcome to MultiModel GPT Space, Here you can freely talk to Kowshan GPT!",
+    multimodal=True,
+    submit_btn="💬 KowshanGPT",
     additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
+        gr.Textbox(
+            value="You are a friendly Chatbot.", 
+            label="System message"
+        ),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
