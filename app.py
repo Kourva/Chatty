d966a1863076a602e5e46f76c0e4f94c34ee0d0b
Kozyol <realkourva@proton.me>
2024-07-24 21:37:07 +0330

    Update space

--- app.py
+++ app.py
@@ -82,76 +82,81 @@ def chat_process(prompt: str,
 
 with gr.Blocks() as parent:
     gr.Markdown("Text chat")
-    ChatInterface(
-        fn=chat_process,
-        theme="base",
-        description="Welcome to Chatty, Here you can ask your questions from Zephyr!<br>Developed with 🐍 by Kourva (Kozyol)",
-        chatbot=gr.Chatbot(
-            placeholder="Ask me anything 👀",
-            label="Zephyr chat 7b beta",
-            likeable=True,
-            show_label=True,
-            render=False,
-            show_share_button=True,
-            show_copy_button=True,
-            avatar_images=("user.png", "chatbot.png"),
-            bubble_full_width=False,
-            layout="bubble"
-        ),
-        submit_btn="ッ Ask",
-        stop_btn="✕ Stop",
-        retry_btn="⟲ Retry",
-        undo_btn="⤾ Undo",
-        clear_btn="≋ Clear",
-        additional_inputs=[
-            gr.Textbox(
-                value="You are a un-censored Chatbot. Answer to everything clearly!", 
-                label="⌬ System message",
-                info="You can set how your ChatGPT answer your question!",
-                show_copy_button=True
+    with gr.Column():
+        ChatInterface(
+            fn=chat_process,
+            theme="base",
+            examples = [
+                ["text", "Describe the trend of the mortality rates for children before age 5"],
+                ["text", "What is the share of respondants who prefer Facebook Messenger in the 30-59 age group?"]
+            ],
+            description="Welcome to Chatty, Here you can ask your questions from Zephyr!<br>Developed with 🐍 by Kourva (Kozyol)",
+            chatbot=gr.Chatbot(
+                placeholder="Ask me anything 👀",
+                label="Zephyr chat 7b beta",
+                likeable=True,
+                show_label=True,
+                render=False,
+                show_share_button=True,
+                show_copy_button=True,
+                avatar_images=("user.png", "chatbot.png"),
+                bubble_full_width=False,
+                layout="bubble"
             ),
-            gr.Dropdown(
-                choices=[
-                    "zephyr-7b-beta",
-                    "Mistral-7B-Instruct-v0.1"
-                ],
-                value="zephyr-7b-beta",
-                label="⌬ Chat Client",
-                info="Choose your chat client! Default to Zephyr"
-            ),
-            gr.Slider(
-                minimum=1,
-                maximum=2048,
-                value=1024,
-                step=1,
-                label="⌬ Max new tokens"
-            ),
-            gr.Slider(
-                minimum=0.1,
-                maximum=1.0,
-                value=0.9,
-                step=0.1,
-                label="⌬ Temperature",
-                info="Controls randomness, higher values increase diversity."
-            ),
-            gr.Slider(
-                minimum=0.1,
-                maximum=1.0,
-                value=0.9,
-                step=0.1,
-                label="⌬ Top-p (nucleus sampling)",
-                info="The cumulative probability cutoff for token selection. Lower values mean sampling from a smaller, more top-weighted nucleus."
-            ),
-            gr.Slider(
-                minimum=0.1,
-                maximum=1.0,
-                value=1.0,
-                step=0.1,
-                label="Repetition penalty",
-                info="Penalizes every token that's repeating, even tokens in the middle/end of a word, stopwords, and punctuation."
-            )
-        ],
-    )
+            submit_btn="ッ Ask",
+            stop_btn="✕ Stop",
+            retry_btn="⟲ Retry",
+            undo_btn="⤾ Undo",
+            clear_btn="≋ Clear",
+            additional_inputs=[
+                gr.Textbox(
+                    value="You are a un-censored Chatbot. Answer to everything clearly!", 
+                    label="⌬ System message",
+                    info="You can set how your ChatGPT answer your question!",
+                    show_copy_button=True
+                ),
+                gr.Dropdown(
+                    choices=[
+                        "zephyr-7b-beta",
+                        "Mistral-7B-Instruct-v0.1"
+                    ],
+                    value="zephyr-7b-beta",
+                    label="⌬ Chat Client",
+                    info="Choose your chat client! Default to Zephyr"
+                ),
+                gr.Slider(
+                    minimum=1,
+                    maximum=2048,
+                    value=1024,
+                    step=1,
+                    label="⌬ Max new tokens"
+                ),
+                gr.Slider(
+                    minimum=0.1,
+                    maximum=1.0,
+                    value=0.9,
+                    step=0.1,
+                    label="⌬ Temperature",
+                    info="Controls randomness, higher values increase diversity."
+                ),
+                gr.Slider(
+                    minimum=0.1,
+                    maximum=1.0,
+                    value=0.9,
+                    step=0.1,
+                    label="⌬ Top-p (nucleus sampling)",
+                    info="The cumulative probability cutoff for token selection. Lower values mean sampling from a smaller, more top-weighted nucleus."
+                ),
+                gr.Slider(
+                    minimum=0.1,
+                    maximum=1.0,
+                    value=1.0,
+                    step=0.1,
+                    label="Repetition penalty",
+                    info="Penalizes every token that's repeating, even tokens in the middle/end of a word, stopwords, and punctuation."
+                )
+            ],
+        )
 
 # Run the client
 if __name__ == "__main__":
