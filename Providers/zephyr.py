550134dc4d399e15ebf34264ee8b35ef07f9b443
Kozyol <realkourva@proton.me>
2024-07-24 18:23:52 +0330

    Update space

--- Providers/zephyr.py
+++ Providers/zephyr.py
@@ -2,17 +2,14 @@
 from huggingface_hub import InferenceClient
 
 # Zephyr chat client
-def zephyr_chat(messages, **kwargs):
+def zephyr_chat(messages, kwargs):
     CLIENT: InferenceClient = InferenceClient(
         "HuggingFaceH4/zephyr-7b-beta"
     )
     # Send request to client
     for chunk in CLIENT.chat_completion(
         messages,
-        max_tokens=max_tokens,
-        temperature=temperature,
-        top_p=top_p,
-        stream=True
+        **kwargs
     ):
         # Add chunks to response 
         response += chunk.choices[0].delta.content
